ea9e2bb966702f2bd3113c4e3dfa8f4353aa5e1a
lh3578_ATT <lucia.hadbavnikova@intl.att.com>
2024-05-26 11:30:12 +0200

    angular app 26.5.2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { TodosComponent } from './components/todos/todos.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TodosComponent],
+  imports: [RouterOutlet, HeaderComponent, TodosComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
